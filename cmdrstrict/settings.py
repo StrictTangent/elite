ac83c05149fbf019439adbbc7d9ad1d3026a6c9f
strict <ducttape87@gmail.com>
2019-09-12 14:10:55 -0700

    added media as a static director

--- cmdrstrict/settings.py
+++ cmdrstrict/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
+)
 
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'assets'),
