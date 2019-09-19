aa44c0b5656dc2ac05879bc3e1e7916e015ed013
strict <ducttape87@gmail.com>
2019-09-19 11:24:27 -0700

    Added about and faq pages

--- cmdrstrict/urls.py
+++ cmdrstrict/urls.py
@@ -15,15 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from . import views
+from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
+    path('faq/', views.faq, name='faq'),
     path('', include('elite.urls')),
     path('elite/', include('elite.urls')),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
